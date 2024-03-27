ec03a6c4c5400dd15baa5addc0e12eebe946f7df
Nikhil <harshita10203040@gmail.com>
2024-03-28 00:49:58 +0530

    add

--- server.js
+++ server.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const cors = require('cors');
+
+
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
+
+app.use(cors(corsOptions))
 
 
 app.use(express.static('public'));
